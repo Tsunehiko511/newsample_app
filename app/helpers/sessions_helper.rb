394a3abb79e2eb80329446c0f9adc8c1034db479
Tunehiko511 <simanezumi1989@gmail.com>
2014-10-05 02:52:14 +0900

    onlinetest

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,10 +39,6 @@ module SessionsHelper
     user == current_user
   end
 
-  def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
-  end
 
   def sign_out
     self.current_user = nil
